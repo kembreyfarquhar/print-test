f1a3459f77147f3091787f41af748bf8d7d814fa
kmcknight1 <katiemcknight1991@gmail.com>
2020-11-09 16:49:31 -0600

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Alert, Button } from "react-native";
+import { StarPRNT } from "react-native-star-prnt";
 
 export default function App() {
+  async function connect(e) {
+    e.preventDefault();
+    try {
+      const connect = await StarPRNT.connect("192.168.1.120", "EscPos", false);
+    } catch (e) {
+      return Alert.alert(
+        "Sorry",
+        "There was an error when trying to connect to your printer",
+        [{ text: "Okay" }]
+      );
+    }
+  }
+
+  let commandsArray = [];
+  commandsArray.push({ appendAlignment: StarPRNT.AlignmentPosition.Center });
+  commandsArray.push({ appendFontStyle: "A" });
+  commandsArray.push({ append: "Star Clothing Boutique\n" });
+  commandsArray.push({ appendLineFeed: 2 });
+  commandsArray.push({ appendAlignment: StarPRNT.AlignmentPosition.Left });
+  commandsArray.push({ appendFontStyle: "B" });
+  commandsArray.push({ append: "Here is some text.\n" });
+  commandsArray.push({ enableInvert: true });
+  commandsArray.push({ appendInvert: "Inverted text\n" });
+  commandsArray.push({
+    appendCutPaper: StarPRNT.CutPaperAction.PartialCutWithFeed,
+  });
+
+  async function print(e) {
+    e.preventDefault();
+    try {
+      let printResult = await StarPRNT.print(
+        "EscPos",
+        commandsArray,
+        "192.168.1.120"
+      );
+      console.log(printResult); // Success!
+    } catch (e) {
+      return Alert.alert(
+        "Sorry",
+        "There was an error when trying to print from your printer",
+        [{ text: "Okay" }]
+      );
+    }
+  }
+
+  async function disconnect(e) {
+    e.preventDefault();
+    try {
+      let disconnected = await StarPRNT.disconnect();
+    } catch (e) {
+      return Alert.alert(
+        "Sorry",
+        "There was an error when trying to disconnect from your printer",
+        [{ text: "Okay" }]
+      );
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Connect to printer" onPress={connect} />
+      <Text>Ready to Print?</Text>
+      <Button title="Press to Print!" onPress={print} />
+      <Text>Disconnect printer.</Text>
+      <Button title="Disconnect" onPress={disconnect} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +77,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
